ed239d04f72f6f0df0082084657858b274f0723f
Asnur Ramdani <asnurramdhani12@gmail.com>
2022-01-09 20:31:25 +0700

    belajar golang

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string
+
+	first_name, last_name := "asnur", "ramdani"
+	fmt.Println(first_name + last_name)
+	fmt.Println(string(first_name[3]))
+}
